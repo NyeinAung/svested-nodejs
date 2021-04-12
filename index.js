cadfcb1ead8e4a6a1886c28749763119b75ae9b7
Nyein Aung <nyeinaung1995@gmail.com>
2021-04-12 12:52:27 +0630

    test

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-require('nnode');
-require('./app');
+// importing express framework
+const express = require("express");
+
+const app = express();
+
+// Respond with "hello world" for requests that hit our root "/"
+app.get("/", function (req, res) {
+    return res.send("Svested");
+});
+
+// listen to port 3000 by default
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server is running");
+});
+
+module.exports = app;
