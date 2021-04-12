be45a69394ad1b75bfaede18e273908841f4dd9f
Nyein Aung <nyeinaung1995@gmail.com>
2021-04-12 14:34:41 +0630

    fix test

--- test/test.js
+++
@@ -1,8 +0,0 @@
-const request = require("supertest");
-const app = require("../index");
-
-describe("GET /", () => {
-    it("respond with Svested App", (done) => {
-    request(app).get("/api/data").expect("Svested", done);
-    })
-});
